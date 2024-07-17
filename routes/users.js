811f68ba5dd3098828ce1144ea351175eb6b6c6e
jenny-ngama <jennygama270319@gmail.com>
2024-07-17 21:16:59 +0200

    add authentification

--- routes/users.js
+++ routes/users.js
@@ -4,15 +4,15 @@ const middlewareAuth = require('../middleware/auth');
 const ctr_adm = require('../controllers/controller_adm');
 
 // API SERVER
-router.post('/server/signup', ctr_adm.serverSignup);
+router.post('/server/signup', middlewareAuth, ctr_adm.serverSignup);
 router.post('/server/login', ctr_adm.serverLogin);
-router.post('/server/logout', ctr_adm.serverLogout);
-router.get('/server', ctr_adm.serverUsersGet);
-router.get('/server/data', ctr_adm.serverUsersJson);
-router.get('/server/create', ctr_adm.serverUserCreate);
-router.get('/server/:requestId', ctr_adm.serverUserGet);
-router.get('/server/data/:requestId', ctr_adm.serverUserGetJson);
-router.put('/server/:requestId', ctr_adm.serverUserPut);
-router.post('/', ctr_adm.serverUsersEmpty);
+router.post('/server/logout', middlewareAuth, ctr_adm.serverLogout);
+router.get('/server', middlewareAuth, ctr_adm.serverUsersGet);
+router.get('/server/data', middlewareAuth, ctr_adm.serverUsersJson);
+router.get('/server/create', middlewareAuth, ctr_adm.serverUserCreate);
+router.get('/server/:requestId', middlewareAuth, ctr_adm.serverUserGet);
+router.get('/server/data/:requestId', middlewareAuth, ctr_adm.serverUserGetJson);
+router.put('/server/:requestId', middlewareAuth, ctr_adm.serverUserPut);
+router.post('/', middlewareAuth, ctr_adm.serverUsersEmpty);
 
 module.exports = router;
