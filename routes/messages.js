48016b028449eec38a309c48c09300daab58e117
jenny-ngama <jennygama270319@gmail.com>
2024-07-19 23:54:03 +0200

    mise a jour du rendu mobile

--- routes/messages.js
+++ routes/messages.js
@@ -5,6 +5,9 @@ const ctr_message = require('../controllers/controller_message');
 
 router.post('/receive', ctr_message.messageReceive);
 router.get('/', middlewareAuth, ctr_message.messagesGet);
+router.put('/', middlewareAuth, ctr_message.messagesPut);
+router.get('/data', middlewareAuth, ctr_message.messagesData);
 router.get('/:id', middlewareAuth, ctr_message.messageGet);
 
 module.exports = router;
+
